fb1e4c2225a9b9001d267141e8233cdf43b6f9df
Marina Natalina <kiska21mk@yandex.ru>
2022-07-20 23:27:06 +0300

    diplom

--- models/movie.js
+++ models/movie.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { validateURL } = require('../utils/const');
 
-const cardSchema = new mongoose.Schema(
+const movieSchema = new mongoose.Schema(
   {
     country: {
       type: String,
@@ -61,4 +61,4 @@ const cardSchema = new mongoose.Schema(
   },
 );
 
-module.exports = mongoose.model('card', cardSchema);
+module.exports = mongoose.model('movie', movieSchema);
