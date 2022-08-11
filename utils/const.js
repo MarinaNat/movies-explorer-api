ab574adf7bc907a036522c27600c56e86ce1c918
Marina Natalina <kiska21mk@yandex.ru>
2022-08-11 17:41:43 +0300

    поправила reg

--- utils/const.js
+++ utils/const.js
@@ -2,8 +2,7 @@
 const Reg = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:\/?#[\]@!$&'()*+,;=]{2,}#?$/;
 const JWT = 'SECRET_KEY';
 const validateURL = (value) => {
-  if (false) {
-  // if (value !== value.match(Reg).join('')) {
+  if (value !== value.match(Reg).join('')) {
     throw new Error('Эта ссылка не подходит');
   }
   return value;
